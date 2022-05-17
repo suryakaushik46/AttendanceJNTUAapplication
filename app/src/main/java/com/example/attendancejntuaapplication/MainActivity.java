8f6a71eb0595b26eaadd925fa4f679790b5dfa2e
suryakaushik46 <99884021+suryakaushikGMP@users.noreply.github.com>
2022-05-17 09:54:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/attendancejntuaapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.attendancejntuaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=(WebView) findViewById(R.id.web);
+        webView.setWebViewClient(new MyBrowser());
+        String url="atdmngsys.000webhostapp.com";
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl(url);
+
+
+    }
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
